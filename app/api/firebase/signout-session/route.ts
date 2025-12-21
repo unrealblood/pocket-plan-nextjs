208af28d807d4d900f4220cd13253e788962abe5
Shubham Singh <shubham110084@outlook.com>
2025-12-21 20:21:47 +0530

    update signout logic and session management

--- app/api/firebase/signout-session/route.ts
+++ app/api/firebase/signout-session/route.ts
@@ -3,9 +3,5 @@ import { cookies } from "next/headers";
 export async function POST(request: Request) {
     (await cookies()).delete("session_token");
 
-    if((await cookies()).get("session_token")) {
-        return Response.json({success: true});
-    }
-
-    return Response.json({success: false});
+    return Response.json({success: true});
 }
