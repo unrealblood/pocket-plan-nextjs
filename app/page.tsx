ed48e74202e8a9b4d1e66ae1c8a7afebfbacbec6
Shubham Singh <shubham110084@outlook.com>
2026-02-04 14:58:48 +0530

    add mobile view to home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import InfoSection from "@/components/home/InfoSection";
 export default function Home() {
   return (
     <div className="flex-1 overflow-auto">
-      <div className="w-[1200px] mx-auto">
+      <div className="sm:w-[1200px] w-[320px] mx-auto">
         <HeroSection />
         <InfoSection />
       </div>
--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -4,11 +4,11 @@ export default async function HeroSection() {
     return (
         <section className="mt-8">
             <div className="text-center shadow-xl ring-1 ring-black/5 rounded-xl p-8">
-                <div className="text-6xl font-bold text-gray-800 px-40">
+                <div className="sm:text-6xl text-4xl font-bold text-gray-800 sm:px-40">
                     Master Your Money. Pocket Your Future
                 </div>
 
-                <p className="text-2xl mt-4 px-60 text-gray-500">Pocket Plan simplifies budgeting, tracking expenses, and planning for your future. Take control of your income, bills, and goals--all in one smart place.</p>
+                <p className="text-2xl mt-4 sm:px-60 text-gray-500">Pocket Plan simplifies budgeting, tracking expenses, and planning for your future. Take control of your income, bills, and goals--all in one smart place.</p>
 
                 <Link href="/dashboard" className="flex justify-center items-center w-56 mx-auto">
                     <div className="bg-teal-500 text-white w-56 px-6 py-4 rounded-full mt-8">
